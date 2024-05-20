6ce63ce010874dfc9212efa1c406302f03674106
andreaoliveira9 <andreaoliveira@ua.pt>
2024-05-20 11:23:00 +0100

    Little changes

--- api/src/main/java/gatemate/Application.java
+++ api/src/main/java/gatemate/Application.java
@@ -3,8 +3,6 @@ package gatemate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@Generated
 @SpringBootApplication
 public class Application {
 
--- api/src/main/java/gatemate/exceptions/GlobalExceptionHandler.java
+++ api/src/main/java/gatemate/exceptions/GlobalExceptionHandler.java
@@ -6,19 +6,15 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import gatemate.Generated;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    @Generated
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<String> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
         Class<?> requiredType = ex.getRequiredType();
         if (requiredType != null && requiredType.isEnum()) {
             return new ResponseEntity<>("Invalid transaction status value", HttpStatus.BAD_REQUEST);
         }
-      
+
         return new ResponseEntity<>("Bad request", HttpStatus.BAD_REQUEST);
     }
 }
